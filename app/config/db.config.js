2d5f3e465b72e21fe087ca0eda40cd25b7084a0b
priyathg <priyath.gregory@trycake.com>
2019-02-18 09:47:25 +0530

    updated db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 const env = require('./env.js');
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-    host: env.host,
-    dialect: env.dialect,
+const sequelize = new Sequelize('heroku_0162d378c620b88', 'bac0b9b188b527', '4665ad1a', {
+    host: 'us-cdbr-iron-east-03.cleardb.net',
+    dialect: 'mysql',
     operatorsAliases: false,
 
     pool: {
